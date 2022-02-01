74192e64dff619dcd533eaf787eb6d7a2bf63605
Shon Feder <shon.feder@gmail.com>
2022-01-31 22:53:02 -0500

    Update dependencies

--- lib/dune_cmd.ml
+++ lib/dune_cmd.ml
@@ -6,9 +6,11 @@ let bin = Cmd.v "dune"
 let build () =
   Cmd_util.run_out Cmd.(bin % "build")
 
+let lib_not_found_re = Str.regexp {|Error: Library .* not found.|}
+
 let warn_on_lib_not_found = function
   | Ok (output, (_, `Exited 1)) as result ->
-    if not Bos.Pat.(matches (v {|Error: Library $(LIB) not found.|}) output)
+    if not (Str.string_match lib_not_found_re output 0)
     then
       OS.Cmd.success result
     else (
